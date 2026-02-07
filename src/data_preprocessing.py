4a4ae8efd7618ee0dc1e9892989aa6b31db805f1
Raghu3696 <168071204+Raghu3696@users.noreply.github.com>
2026-02-07 12:54:33 +0530

    Delete src directory

--- src/data_preprocessing.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def load_data(file_path):
-    """Load cyber threat dataset"""
-    return pd.read_csv(file_path)
-
-def clean_data(df):
-    """Basic cleaning step"""
-    return df.dropna()
